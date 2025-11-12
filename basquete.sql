2a0033fffbd4f56195f6ffd3443ff962eb05d6b4
JGfax <joao.bicudo@aluno.senai.br>
2025-11-12 09:44:35 -0300

    jhg

--- basquete.sql
+++ basquete.sql
@@ -1,7 +1,7 @@
 create database basquete;
 use basquete;
 
-create table usuario (
+create table jogador (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100),
     senha VARCHAR(255),  -- Aumenta o tamanho da coluna para 255 caracteres
--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+include 'conexao.php';
+
+// Busca os serviços do banco
+$sql = "SELECT id_servico, nome_servico FROM servicos ORDER BY nome_servico";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -8,6 +16,49 @@
 </head>
 <body> 
 
+<header>
+  
+<!-- LOGO -->
+
+          <div class="logo">BASKET<span>BALL</span></div>
+  
+<!-- MENU HAMBURGUE-->
+
+          <nav class="menu">
+                <div class="hamburger" onclick="toggleMenu()">☰</div>
+                    <ul>
+                        <li><a href="index.html">Home &nbsp |</a></li>
+                        <li><a href="servicos.html">Serviços &nbsp |</a></li>
+                        <li><a href="contato.html">Contato</a></li>
+                    </ul>
+              </nav>
+
+<!-- MENU MOBILE-->
+
+            <div class="mobile-menu" id="mobileMenu">
+              <a href="#">Home</a>
+              <a href="#">Sobre</a>
+              <a href="#">Serviços</a>
+              <a href="#">Contato</a>
+            </div>
+</header>
+
+        <script>
+            const menu = document.getElementById("mobileMenu");
+
+                function toggleMenu() {
+                menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+                }
+
+                window.addEventListener("resize", () => {
+                    if (window.innerWidth > 768) {
+                        menu.style.display = "none";
+                    }
+                }
+                );
+        </script>
+
+
   <!-- Formulário de login -->
 
   <h2>Formulário</h2>
@@ -15,8 +66,15 @@
 
 
     <label>Equipe:</label><br>
-    <input type="text" name="equipe" required><br>
+    <select id="equipe" name="equipe" required> 
+       <?php while ($row = $result->fetch_assoc()): ?>                   <!-- Loop para preencher as opções do select -->
+                    <option value="<?= $row['id_servico'] ?>">           <!-- apresentar o valor da option -->
+                        <?= htmlspecialchars($row['nome_servico']) ?>    <!-- Exibição do texto na option, de forma automatizada -->
+                    </option>
+                  <?php endwhile; ?>
+    </select>
 
+    
     <label>Jogador:</label><br>
     <input type="text" name="jogador" required><br>
 
@@ -28,8 +86,9 @@
 
      <label>Rebotes por partida:</label><br>
     <input type="number" name="rebotes" required><br>
-    <button type="submit">Cadastrar</button>
 
+
+    <button type="submit">Cadastrar</button>
   </form>
 
 
